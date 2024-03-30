de467c519656c327696183b7fc737c1e5a877296
Ahmet Can Bozkurt <122645167+wixisoftware@users.noreply.github.com>
2024-03-30 13:29:47 +0300

    Upload_4

---
+++ EczaneFramework.MvcWebUI/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using EczaneFramework.Business.Abstract;
+using EczaneFramework.Entities.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EczaneFramework.MvcWebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IEczane _eczaneService;
+        public HomeController(IEczane eczaneService)
+        {
+            _eczaneService = eczaneService;
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Eczaneler()
+        {
+            return View(_eczaneService.GetAll());
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]   
+        public ActionResult Create(Eczane eczane)
+        {
+
+            _eczaneService.Add(eczane);
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(Eczane eczane)
+        {
+
+            _eczaneService.Update(eczane);
+            return View();
+        }
+    }
+}
---
+++ EczaneFramework.MvcWebUI/Controllers/KullaniciController.cs
@@ -0,0 +1,61 @@
+﻿using EczaneFramework.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EczaneFramework.Core.Security;
+using System.Threading;
+using System.Security.Principal;
+using EczaneFramework.Core.Security.Models;
+using Newtonsoft.Json;
+
+
+namespace EczaneFramework.MvcWebUI.Controllers
+{
+    public class KullaniciController : Controller
+    {
+
+        IKullanici _kullaniciServis;
+
+        public KullaniciController(IKullanici kullaniciServis)
+        {
+            _kullaniciServis = kullaniciServis;
+        }
+        
+        
+        // GET: Kullanici
+        public ActionResult Index()
+        {
+            return View(_kullaniciServis.GetAll());
+        }
+
+        public ActionResult Login()
+        {
+
+            var identity = new Identity();
+            identity.AuthenticationType = "Forms";
+            identity.Email = "husrev@yildiz.com";
+            identity.Name = "Husrev";
+            identity.Soyad = "YILDIZ";
+            identity.Ad = "Hüsrev";
+            identity.Roles = new string[] { "Admin", "User" };
+
+            var stringIdentity = JsonConvert.SerializeObject(identity);
+            HttpCookie identityCookie = new HttpCookie("Account", stringIdentity);
+           
+            Response.Cookies.Add(identityCookie);
+           
+            
+
+
+
+            // _kullaniciServis.GenericIsLogin("husrevyildiz@gmail.com", "123123");
+
+
+            //ISecurityHelper s = new SessionSecurity();
+            //var r = s.GetAccount();
+            return View();
+        }
+    }
+}

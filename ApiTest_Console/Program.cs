28ce64f5f07b01df7b89b007cd1372820cf1831a
Ahmet Can Bozkurt <122645167+wixisoftware@users.noreply.github.com>
2024-03-30 13:21:37 +0300

    Upload_1

---
+++ ApiTest_Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTest_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetOperation();
+            PostOperation();
+           
+
+        }
+
+        private static void PostOperation()
+        {
+            var client = new RestClient("http://localhost:53629/api/Eczane/");
+
+
+            var req = new RestRequest(Method.GET);
+            Eczane ecz = new Eczane();
+            ecz.Id = 99;
+            ecz.Ad = "Güzeller";
+
+            req.AddJsonBody(ecz);
+
+            var result = client.Execute(req);
+            
+            if (result.ContentLength > 0)
+            {
+                Console.WriteLine(result.Content.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        public class Eczane
+        {
+            public int Id { get; set; }
+            public string Ad { get; set; }
+            public string Adress { get; set; }
+            public string Telefon { get; set; }
+            public string Yetkili { get; set; }
+            public string Mail { get; set; }
+
+        }
+
+        private static void GetOperation()
+        {
+            var client = new RestClient("http://localhost:53629/api/Eczane/");
+
+
+            var req = new RestRequest(Method.GET);
+
+            var result = client.Execute(req);
+
+            if (result.ContentLength > 0)
+            {
+                Console.WriteLine(result.Content.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EczaneFramework.APIUI/Controllers/EczaneController.cs
@@ -0,0 +1,48 @@
+﻿using EczaneFramework.Entities.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EczaneFramework.APIUI.Controllers
+{
+    public class EczaneController : ApiController
+    {
+        List<Eczane> fakeData = new List<Eczane>()
+            {
+                new Eczane { Id=15,Ad="eczane 1" },
+                new Eczane { Id=16,Ad="eczane 2" },
+                new Eczane { Id=17,Ad="eczane 3" }
+                
+            };
+
+
+        [HttpGet]
+        public List<Eczane> Get()
+        {
+            return fakeData;
+        }
+      
+
+        [HttpPut]
+        public Eczane Post(int Id)
+        {
+            return fakeData.FirstOrDefault(h => h.Id == Id);
+        }
+
+        [HttpPost]
+        public Eczane Add(Eczane eczane)
+        {
+            return eczane;
+        }
+
+        [HttpDelete]
+        public Eczane Delete(Eczane eczane)
+        {
+            return eczane;
+        }
+
+    }
+}
